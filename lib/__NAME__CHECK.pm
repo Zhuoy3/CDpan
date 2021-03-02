8ec08f6ff40fce805d744877e50887f9bfa31ddf
Zhuo Y <zhuo0000yue@gmail.com>
2021-03-02 22:02:57 +0800

    0.0.0

---
+++ lib/__NAME__CHECK.pm
@@ -0,0 +1,16 @@
+###
+# Description:
+# Author: Zhuo Yue
+# Date: 2021-03-02 21:56:27
+# LastEditors: Zhuo Yue
+# LastEditTime: 2021-03-02 21:57:56
+# Calls:
+# Called By:
+# FilePath: \__NAME__\lib\__NAME__CHECK.pm
+###
+
+package check;
+
+
+
+1;

f848bccdf5a798d1032cba6048a7ca4a29688bae
Zhuo Y <zhuo0000yue@gmail.com>
2021-03-08 18:53:47 +0800

    0.0.1

---
+++ lib/NAME/Check.pm
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+# Description:
+# Author: Zhuo Yue
+# Date: 2021-03-02
+
+
+package Check;
+
+use strict;
+use warnings;
+
+1;
---
+++ lib/NAME/QualityControl.pm
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+# Description:
+# Author: Zhuo Yue
+# Date: 2021-03-02
+
+package QualityControl;
+
+use strict;
+use warnings;
